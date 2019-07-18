4875c18249b7095bc0e260630bcbbd8f85dd3b15
yoshida <yoshida.eth0@gmail.com>
2019-07-18 18:15:39 +0900

    add filter files

---
+++ lib/synthesizer/filter.rb
@@ -0,0 +1,14 @@
+require 'synthesizer/filter/low_pass_filter'
+require 'synthesizer/filter/high_pass_filter'
+require 'synthesizer/filter/band_pass_filter'
+require 'synthesizer/filter/low_shelf_filter'
+require 'synthesizer/filter/high_shelf_filter'
+require 'synthesizer/filter/peaking_filter'
+
+require 'synthesizer/filter/serial'
+require 'synthesizer/filter/parallel'
+
+module Synthesizer
+  module Filter
+  end
+end

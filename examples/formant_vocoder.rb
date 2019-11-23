fa601125d7a098b54bf1b843997b961e1378b3fe
yoshida <yoshida.eth0@gmail.com>
2019-11-24 00:37:22 +0900

    mod example formantvocoder

--- examples/formant_vocoder.rb
+++ examples/formant_vocoder.rb
@@ -72,7 +72,7 @@ bufs += 50.times.map {|_| synth.next}
 track1 = AudioInput.buffer(bufs)
 
 stereo_out = AudioOutput.device(soundinfo: soundinfo)
-#stereo_out = AudioOutput.file("formatvocoder.wav", soundinfo: soundinfo)
+#stereo_out = AudioOutput.file("formant_vocoder.wav", soundinfo: soundinfo)
 
 track1
   .send_to(stereo_out, gain: 1.0)
--- examples/formant_vocoder_sweep.rb
+++ examples/formant_vocoder_sweep.rb
@@ -62,7 +62,7 @@ bufs += 50.times.map {|_| synth.next}
 track1 = AudioInput.buffer(bufs)
 
 stereo_out = AudioOutput.device(soundinfo: soundinfo)
-#stereo_out = AudioOutput.file("formatvocoder_sweep.wav", soundinfo: soundinfo)
+#stereo_out = AudioOutput.file("formant_vocoder_sweep.wav", soundinfo: soundinfo)
 
 track1
   .send_to(stereo_out, gain: 1.0)
